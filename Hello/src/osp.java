e4af339d6cd437e4cb4a0b5949ba5004862c6b75
subin <bin2042@naver.com>
2021-04-01 16:13:40 +0900

    added 'her world'

--- Hello/src/osp.java
+++ Hello/src/osp.java
@@ -4,6 +4,7 @@ public class osp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello his World");
+		System.out.println("Hello her World");
 	}
 
 }
